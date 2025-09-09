0eddac35a023bd73213759417c4827a434a8f81b
Nata Parahita <nata_anindya_34rpl@student.smktelkom-mlg.sch.id>
2025-09-09 09:56:53 +0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+public class Car {
+    // property class
+    int  numberOfDoors;
+    boolean isElectric;
+
+    //Constructor
+    public Car() {
+        numberOfDoors = 4;
+        isElectric = false;
+    }
+
+    //getter
+        //setter
+        //method
+        public void print() {
+            System.out.println("Number of Doors: " + numberOfDoors);
+            System.out.println("Is Electric: " + isElectric);
+        }
+
+    }
+
---
+++ CarTest.java
@@ -0,0 +1,16 @@
+
+
+public class CarTest {
+   public static void main(String[] args) {
+        Car car  = new Car();
+        car.print();
+
+        Car truck = new Car();
+        truck.print();
+
+       
+
+    }
+    }
+    
+
